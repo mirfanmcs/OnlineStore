5f903a386787d1d1c1a58776c1e0fc887b5d1842
mirfanmcs <contact@irfanm.com>
2017-10-15 16:14:28 +1100

    added missing file

---
+++ OnlineStoreWebApi/Settings/OnlineStoreMqSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStoreWebApi.Settings
+{
+    public class OnlineStoreMqSettings
+    {
+        public string HostName { get; set; }
+        public string ExchangeName { get; set; }
+        public string ExchhangeType { get; set; }
+        public string QueueName { get; set; }
+        public string RouteKey { get; set; } 
+    }
+}
